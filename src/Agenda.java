f5be39e3c769237c4cfd19f34c2221fe4d894845
JANVER77 <janver77@gmail.com>
2025-12-18 10:39:41 -0500

    Creando clases/ funciones/metodos

--- src/Agenda.java
+++ src/Agenda.java
@@ -1,2 +1,48 @@
+import java.util.ArrayList;
+
 public class Agenda {
+    private ArrayList<Contacto> agenda = new ArrayList<>();
+
+    Contacto[]  agenda2 = new Contacto[10];
+
+
+    public Agenda(ArrayList<Contacto> agenda) {
+        this.agenda = agenda;
+    }
+
+    public ArrayList<Contacto> getAgenda() {
+        return agenda;
+    }
+
+    public void setAgenda(ArrayList<Contacto> agenda) {
+        this.agenda = agenda;
+    }
+
+    public void añadirContacto(Contacto C){
+
+    }
+    public void existeContacto(Contacto C){
+
+    }
+    public void listarContactos(){
+
+    }
+    public void buscarContacto(String nombre){
+
+    }
+    public void eliminarContacto(Contacto C){
+
+    }
+    public void modificarTelefono(Contacto C){
+
+    }
+    public void agendaLLena(){
+
+    }
+    public void espaciosLibres(){
+
+    }
+
 }
+
+
