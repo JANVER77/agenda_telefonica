e549d770d83b26a72648556ec152f9b7956c1c92
JANVER77 <janver77@gmail.com>
2025-12-18 11:53:45 -0500

    funcionalidad

--- src/Agenda.java
+++ src/Agenda.java
@@ -1,31 +1,39 @@
 import java.util.ArrayList;
 
 public class Agenda {
-    private ArrayList<Contacto> agenda = new ArrayList<>();
+    //private ArrayList<Contacto> agenda = new ArrayList<>();
 
     Contacto[]  agenda2 = new Contacto[10];
 
-
-    public Agenda(ArrayList<Contacto> agenda) {
-        this.agenda = agenda;
+    public Agenda(Contacto[] C) {
+        this.agenda2 = C;
     }
 
-    public ArrayList<Contacto> getAgenda() {
-        return agenda;
+    public Contacto[] getAgenda2() {
+        return agenda2;
     }
 
-    public void setAgenda(ArrayList<Contacto> agenda) {
-        this.agenda = agenda;
+    public void setAgenda2(Contacto[] agenda2) {
+        this.agenda2 = agenda2;
     }
 
     public void añadirContacto(Contacto C){
-
+        int tam_agenda = 0;
+        for(Contacto c : agenda2){
+            tam_agenda++;
+        }
+        agenda2[tam_agenda+1] = C;
     }
     public void existeContacto(Contacto C){
 
     }
     public void listarContactos(){
-
+        int cantContatactos = 0;
+        for(int i = 0; i < agenda2.length; i++){
+            if(agenda2[i] != null){
+                System.out.println(agenda2[i].getNombre());
+            }
+        }
     }
     public void buscarContacto(String nombre){
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        Contacto contacto1 = new Contacto("Elker", "Herrera", "3003158254");
+        Contacto contacto2 = new Contacto("Ana", "Herrera", "3003158254");
+        Contacto contacto3 = new Contacto("Daniel", "Herrera", "3003158254");
+        Contacto contacto4 = new Contacto("Jeffersson", "Herrera", "3003158254");
+
+        Contacto contacto5 = new Contacto();
+
+
+        Contacto[] agenda1 = new Contacto[10];
+
+        agenda1[0] = contacto1;
+        agenda1[1] = contacto2;
+        agenda1[2] = contacto3;
+
+        System.out.println(contacto2.getNombre());
+
+        Agenda miAgenda = new Agenda(agenda1);
+
+        miAgenda.listarContactos();
+
+        miAgenda.añadirContacto(contacto4);
+
 
     }
 }
